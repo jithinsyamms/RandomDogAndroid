f31478d7b98ec70be7f73041434e8e27711add0a
jithin <jithinsyamms@gmail.com>
2022-01-15 17:25:10 +0530

    Initial commit

---
+++ app/src/main/java/com/jithinsyamms/randomdog/RecentDogsActivity.java
@@ -0,0 +1,17 @@
+package com.jithinsyamms.randomdog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RecentDogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recent_dogs);
+
+    }
+}
