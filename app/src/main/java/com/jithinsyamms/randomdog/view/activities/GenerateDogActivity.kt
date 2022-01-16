d66c041148f8d4f4a11414378896ae18724cdcf3
jithin <jithinsyamms@gmail.com>
2022-01-16 23:03:35 +0530

    Saved bitmap during rotation, added landscape layouts

--- app/src/main/java/com/jithinsyamms/randomdog/view/activities/GenerateDogActivity.kt
+++ app/src/main/java/com/jithinsyamms/randomdog/view/activities/GenerateDogActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jithinsyamms.randomdog.R
 import com.jithinsyamms.randomdog.image.ImageListener
+import com.jithinsyamms.randomdog.utils.Utils
 import com.jithinsyamms.randomdog.view.viewmodel.DogViewModel
 
 
@@ -16,20 +17,38 @@ class GenerateDogActivity : AppCompatActivity(), ImageListener {
     private var dogImageView: ImageView? = null
     private var dogViewModel = DogViewModel(this)
     private lateinit var generateButton:Button
+    private var downloadedImage: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_dog)
         dogImageView  = findViewById(R.id.dogImageView)
         generateButton = findViewById(R.id.generate)
+
+        if (savedInstanceState != null) {
+            val imageBytes = savedInstanceState.getByteArray("BitmapImage")
+            if (imageBytes != null) {
+                downloadedImage = Utils.getImage(imageBytes)
+                dogImageView?.setImageBitmap(downloadedImage)
+            }
+        }
+
         generateButton.setOnClickListener {
             generateButton.isEnabled = false
             dogViewModel.fetchRandomDog()
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (downloadedImage != null){
+            outState.putByteArray("BitmapImage", Utils.getByteArrayFromBitmap(downloadedImage!!))
+        }
+    }
+
     override fun imageDownloaded(image: Bitmap) {
         dogImageView?.setImageBitmap(image)
+        downloadedImage = image
         generateButton.isEnabled = true
     }
 
@@ -37,4 +56,8 @@ class GenerateDogActivity : AppCompatActivity(), ImageListener {
         generateButton.isEnabled = true
         Toast.makeText(this,"Download Error", Toast.LENGTH_SHORT).show()
     }
+    
+    
+
+
 }
